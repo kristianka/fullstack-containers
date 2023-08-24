5707c3864d33cd5ffe33b628fcc7f4b705c7f10c
Kristian <49764796+kristianka@users.noreply.github.com>
2023-08-24 16:31:58 +0300

    update redis

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
 const { MONGO_URL } = require('../util/config')
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-
+if (MONGO_URL && !mongoose.connection.readyState)
+  try {
+    mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  } catch (error) {
+    console.log('error connecting to MongoDB:', error.message)
+  }
 
 module.exports = {
   Todo
